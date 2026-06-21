304aea015f9c1bc4c5b99daadd0ff7d6bcdb024d
Felipe-Flores <felipe.floresceo@gmail.com>
2026-06-21 15:18:55 -0400

    feat: adicionado a tela de consulta de processo por tabela

--- src/view/ConsultaProcessoTabelaDialog.java
+++ src/view/ConsultaProcessoTabelaDialog.java
@@ -1,50 +1,54 @@
 package view;
 
-import database.dao.DirecaoDAO;
+import database.dao.ProcessoTabelaDAO;
 import database.model.TB_REPLICACAO_DIRECAO;
+import database.model.TB_REPLICACAO_PROCESSO_TABELA;
 
 import javax.swing.*;
 import javax.swing.table.DefaultTableModel;
-import java.awt.event.MouseEvent;
+import java.awt.*;
 import java.sql.SQLException;
 import java.util.ArrayList;
 
-public class ConsultaDirecaoDialog extends JDialog{
+public class ConsultaProcessoTabelaDialog extends JDialog {
 
     private JTable table;
     private JButton btnSelecionar;
     private JButton btnCancelar;
 
-    private TB_REPLICACAO_DIRECAO selecionado;
-    private DirecaoDAO dao;
 
-    public ConsultaDirecaoDialog(JFrame parent, DirecaoDAO dao) throws Exception {
-        super(parent, "Consulta Direçao", true);
-        setSize(900,420);
+    private TB_REPLICACAO_PROCESSO_TABELA selecionado;
+
+    public ConsultaProcessoTabelaDialog(Frame parent, ProcessoTabelaDAO dao) throws Exception {
+        super(parent, "Consulta Tabela", true);
+        setSize(1000, 420);
         setLocationRelativeTo(parent);
         setLayout(null);
 
         DefaultTableModel model = new DefaultTableModel();
         model.addColumn("ID");
         model.addColumn("PROCESSO_ID");
+        model.addColumn("TABELA_ORIGEM");
+        model.addColumn("TABELA_DESTINO");
         model.addColumn("ORIGEM");
-        model.addColumn("DESTINO");
         model.addColumn("HABILITADO");
 
-        ArrayList<TB_REPLICACAO_DIRECAO> lista = this.dao.selectAll();
-        for (TB_REPLICACAO_DIRECAO p: lista){
+        ArrayList<TB_REPLICACAO_PROCESSO_TABELA> lista = dao.selectAll();
+        for (TB_REPLICACAO_PROCESSO_TABELA t : lista) {
             model.addRow(new Object[]{
-                    p.getId(),
-                    p.getDirecao_destino(),
-                    p.getDirecao_origem(),
-                    p.isHabilitado()
+                    t.getId(),
+                    t.getProcesso_id(),
+                    t.getTabela_origem(),
+                    t.getTabela_destino(),
+                    t.getOrdem(),
+                    t.isHabilitado()
             });
         }
 
         table = new JTable(model);
-        JScrollPane scrollPane = new JScrollPane(table);
-        scrollPane.setBounds(10,10,860,300);
-        add(scrollPane);
+        JScrollPane scroll = new JScrollPane(table);
+        scroll.setBounds(10,10,860,300);
+        add(scroll);
 
         btnSelecionar = new JButton("SELECIONAR");
         btnSelecionar.setBounds(10,320,140,30);
@@ -55,38 +59,38 @@ public class ConsultaDirecaoDialog extends JDialog{
         add(btnCancelar);
 
         btnCancelar.addActionListener(e -> {
-            selecionado= null;
+            selecionado = null;
             dispose();
         });
 
-        btnSelecionar.addActionListener(e -> {
+        btnSelecionar.addActionListener( e ->{
             int row = table.getSelectedRow();
-            if (row == -1){
-                JOptionPane.showMessageDialog(this,"Selecione uma linha.");
+            if(row == -1){
+                JOptionPane.showMessageDialog(this, "Selecione uma linha.");
                 return;
             }
 
-            long id = Long.parseLong(table.getValueAt(row,0).toString());
+            long id = Long.parseLong(table.getValueAt(row, 0).toString());
             TB_REPLICACAO_DIRECAO d = null;
-            try {
+            try{
                 d = dao.selectById(id);
-            }catch (SQLException ex) {
+            }catch (SQLException ex){
                 throw new RuntimeException(ex);
             }
+
             selecionado = d;
             dispose();
         });
 
         table.addMouseListener(new java.awt.event.MouseAdapter(){
-            @Override
-            public void mouseClicked(MouseEvent e ) {
-                if (e.getClickCount() == 2) {
+            public void mouseClicked(java.awt.event.MouseEvent evt){
+                if (evt.getClickCount() == 2){
                     btnSelecionar.doClick();
                 }
             }
         });
     }
-    public TB_REPLICACAO_DIRECAO getSelecionado() {
+    public TB_REPLICACAO_PROCESSO_TABELA getSelecionado() {
         return selecionado;
     }
 }
